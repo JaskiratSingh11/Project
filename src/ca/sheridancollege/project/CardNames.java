7a642368f16b419d285a43b30d64172a25ec8948
JaskiratSingh11 <singh891@sheridancollege.ca>
2024-10-09 16:35:18 -0400

    project

---
+++ src/ca/sheridancollege/project/CardNames.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author singh891
+ */
+public enum CardNames {
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+    
+    private final int value;
+
+    CardNames(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
